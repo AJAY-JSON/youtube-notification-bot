6e259a2cea5faa41d9054a8bfd343f6cdaf15ee8
AJAY-JSON <135541307+AJAY-JSON@users.noreply.github.com>
2024-05-17 19:18:38 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "",
+    token: "Token Im secret",
     channel: "1240578141236236288",
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
     channel_id: "UCUl9VsRo6_HOwXdx-SPtrLQ",
