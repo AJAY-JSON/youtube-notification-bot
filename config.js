adac1ef09e2f3ae4a6d2e11a50af5955ef8fd9e4
AJAY-JSON <135541307+AJAY-JSON@users.noreply.github.com>
2024-05-17 19:18:05 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "YOUR_DISCORD_BOT_TOKEN",
-    channel: "XXXXXXXXXXXXXXXXXX",
+    token: "",
+    channel: "1240578141236236288",
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
-    channel_id: "YOUTUBE_CHANNEL_ID",
+    channel_id: "UCUl9VsRo6_HOwXdx-SPtrLQ",
     watchInterval: 30000
 }
